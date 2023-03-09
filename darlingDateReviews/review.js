728503070ec4b299d27893d77c4c0082b21c8c45
Eliza Kartchner <eliza.kartchner@gmail.com>
2023-03-08 21:34:24 -0700

    forgot to add this file

--- darlingDateReviews/review.js
+++ darlingDateReviews/review.js
@@ -18,14 +18,32 @@ class CreateReview {
     cacheReviewInfo() {
         const dateIdeaElement = document.querySelector("#newDateIdea");
         const dateInfoElement = document.querySelector("#infoDateIdea");
-        localStorage.setItem("dateIdea", dateIdeaElement.value);
-        localStorage.setItem("dateInfo", dateInfoElement.value);
-        window.location.href = "topDates.html";
+
+        const dateIdea = dateIdeaElement.value;
+        const dateInfo = dateInfoElement.value;
+
+        let dateObject = [];
+        const dateText = localStorage.getItem('dates');
+        
+        if (dateText) {
+            dateObject = JSON.parse(dateText);
+        }
+
+        const newIdea = { idea: dateIdea, info: dateInfo };
+        dateObject.push(newIdea);
+        console.log(dateObject);
+
+        localStorage.setItem('dates', JSON.stringify(dateObject));
 
         // set the value to blank 
         dateIdeaElement.value = "";
         dateInfoElement.value = "";
+
+        //localStorage.setItem("dateIdea", dateIdeaElement.value);
+        //localStorage.setItem("dateInfo", dateInfoElement.value);
+        //window.location.href = "topDates.html";
     }
+
 
 }
 
