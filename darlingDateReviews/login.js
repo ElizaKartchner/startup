aa896c321740ae2e782a0cae007867704c76b18d
Eliza <eliza.kartchner@gmail.com>
2023-03-29 21:24:59 -0600

    rearrange files to be in public and website folders

--- darlingDateReviews/login.js
+++
@@ -1,7 +0,0 @@
-function login() {
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "about.html";
-    window.location.href = "review.html";
-    window.location.href = "topDates.html";
-}
