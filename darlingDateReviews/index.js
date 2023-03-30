e7c719aaa1144c06da41ef31d320a1d634f62ce3
Eliza <eliza.kartchner@gmail.com>
2023-03-29 20:07:35 -0600

    add picture and quote to about

---
+++ darlingDateReviews/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+// The service port. In production the application is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the application's static content
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// GetDateIdeas
+apiRouter.get('/dateIdeas', (_req, res) => {
+  res.send(ideas);
+});
+
+// SubmitDateIdeas
+apiRouter.post('/dateIdeas', (req, res) => {
+  res.send(ideas);
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
